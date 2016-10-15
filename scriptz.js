eac923aa9870bcd64594f008fe6751e0492516b3
Phillip Salazar <areabio52@gmail.com>
2016-10-14 19:35:38 -0500

    version 1.0.1

--- scriptz.js
+++ scriptz.js
@@ -3,34 +3,29 @@
 
 
 $(document).ready(function(){
-
-  /*chrome.windows.create({
-      url:"popup.html",
-      type:"panel",
-      width:300,
-      height:200
-      */
-
+var i = 0;
 
 $('#formatbutton').click(function(){
   var $tow = $('#ToW').val();
    // 2
   var $aow = $('#AoW').val();
     // 3
-  var $wu =  $('#WU').val();
+
     // 4
   var $pub = $('#Pub').val();
     // 5
   var $da =  $('#DA').val();
+  var a;
+  if (i === 1){
+      var a = "print";
+  }else if (i === 2){
+    var a = "web";
+  }else {
+    a = "";
+  }
 
-document.getElementById('p1').innerHTML = $tow + " " + $aow + " " + $wu +" " + $pub + " " + $da;
-/*
-document.getElementById('p2').innerHTML = $aow;
-document.getElementById('p3').innerHTML = $wu;
-document.getElementById('p4').innerHTML = $pub;
-document.getElementById('p5').innerHTML = $da;
-*/
-//$('#results').append($txt0);
+document.getElementById('p1').innerHTML = $tow + ", " + $aow  +", " + $pub + ", " + a +", " + $da;
+
 
 });
 
@@ -38,7 +33,7 @@ $('#removebutton').click(function(){
   // empyty paragraph
   document.getElementById('ToW').value = "";
   document.getElementById('AoW').value = "";
-  document.getElementById('WU').value = "";
+
   document.getElementById('Pub').value = "";
   document.getElementById('DA').value = "";
 
@@ -54,5 +49,33 @@ catch(err){
 
 });
 
+  $('#print').click(function(){
+      $(this).css({'background-color':'grey'});
+    $('#web').css({'background-color':'white'});
+    i = 1;
+
+
+    return i;
+  });
+
+  $('#print').dblclick(function(){
+    $(this).css({'background-color':'white'});
+    i = 0;
+    return i;
+  });
+
+  $('#web').dblclick(function(){
+    $(this).css({'background-color':'white'});
+    i = 0;
+    return i;
+  });
+
+  $('#web').click(function(){
+    $(this).css({'background-color':'grey',});
+    $('#print').css({'background-color':'white'});
+
+    i = 2;
+    return i;
+  });
 
 });
